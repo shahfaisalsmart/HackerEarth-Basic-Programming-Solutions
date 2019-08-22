f74bfce8ea8a2d6efe8417e7e25e236102bac917
Mohammad Faisal <shahfaisalsmart1234@gmail.com>
2019-08-22 23:43:16 +0530

    Solved Successfully

---
+++ conject_It.cpp
@@ -0,0 +1,19 @@
+// Author: Mohammad Faisal
+/*
+  conject-it!
+  dated: 22-Aug-2019
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        long long s;
+        cin >> s;
+        cout<<"YES"<<endl;
+    }
+    return 0;
+}
