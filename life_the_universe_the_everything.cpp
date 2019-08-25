221aa0a7f6e6f18577f8514789b346bb23e1a31c
Mohammad Faisal <shahfaisalsmart1234@gmail.com>
2019-08-25 17:20:56 +0530

    Solved Successfully

---
+++ life_the_universe_the_everything.cpp
@@ -0,0 +1,18 @@
+// Author: Mohammad Faisal
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    while(cin >> n)
+    {
+        if(n==42)
+            break;
+        cout<<n<<endl;
+    }
+    return 0;
+}
